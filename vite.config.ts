c74016ed2c9ab8fc2b171930753e36204d02c388
benedictthomasm <benedictthomasm@clarium.tech>
2025-10-09 18:11:56 +0530

    removed meta and headers in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    headers: {
-      "Content-Security-Policy": "style-src 'self' https://cdn-uicons.flaticon.com https://fonts.googleapis.com 'unsafe-inline'"
-    }
-  }
+  plugins: [react(),tailwindcss()]
 })
